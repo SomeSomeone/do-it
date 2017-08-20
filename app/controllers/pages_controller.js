37b3f6c163b09f3ab9e7129425121d2f21104085
SomeSomeone <nice.gl_orl@ukr.net>
2017-08-20 19:57:43 +0300

    Front path of map

--- app/controllers/pages_controller.js
+++ app/controllers/pages_controller.js
@@ -1,11 +1,22 @@
 module.exports = function(app) {
-	
+	var DG = require('2gis-maps');	
 
 	app.controller('controller', function($scope) {
-	    $scope.name = "John Doe";
-		
+		$scope.map ="";
+		$scope.marker;
+		$scope.markers = DG.featureGroup(),
+		$scope.markersCount=function() {
+			return Object.keys($scope.markers._layers).length
+		}
 
-		$scope.InitMap = function (){
+		$scope.markersArray=function() {
+			return Object.values($scope.markers._layers) 
+		}
+		
+		$scope.bindPopup = ""
+		$scope.newMarker = { latlng:[0,0], bindPopup:"" };
+		
+		$scope.initMap = function (){
 			getLocation()
 			function getLocation() {
 			    if (navigator.geolocation) {
@@ -18,15 +29,76 @@ module.exports = function(app) {
 		   		var coords = [position.coords.latitude ,position.coords.longitude]; 
 		   		var map
 		   		DG.then(function () {
-		        	map = DG.map('map', {
+		        	$scope.map = DG.map('map', {
 			            center:coords,
 			            zoom: 13
 			        });
-			        DG.marker(coords).addTo(map).bindPopup('Youo');
+			        DG.marker(coords).addTo($scope.map).bindPopup('You');
+			        $scope.map.on('click', function(e) {
+		                $scope.newMarker={ lat: e.latlng.lat , lng: e.latlng.lng};
+		                console.log(e)
+		            });
 			    });
+			}	    
+		}
+
+		$scope.initFullMap = function (){
+			getLocation()
+			function getLocation() {
+			    if (navigator.geolocation) {
+			        navigator.geolocation.getCurrentPosition(showPosition);
+			    } else {
+			        alert("Geolocation is not supported by this browser.");
+			    }
+			}			
+			function showPosition(position) {
+		   		var coords = [position.coords.latitude ,position.coords.longitude]; 
+	        	$scope.map = DG.map('map', {
+		            center:coords,
+		            zoom: 13
+		        });
+		        DG.marker(coords).addTo($scope.map).bindPopup('You here!');
+		        
+		        $scope.map.on('click', $scope.refresh)
+				$scope.showMarker()
 			}
-		    
-		}		
+		}
+
+		$scope.refresh=function(e){
+			console.log($scope.marker)
+		    if ($scope.marker) {
+		    	$scope.marker.setLatLng(e.latlng)
+		    }else{
+		    	$scope.marker = DG.marker(e.latlng).addTo($scope.map);
+		    }
+		    $scope.newMarker.latlng=[e.latlng.lat,e.latlng.lng]
+		    $scope.$apply();
+		}
+
+		$scope.clearMarker=function () {
+			$scope.marker.remove()
+			$scope.newMarker = { latlng: [0,0] , bindPopup:"" };
+			$scope.marker=false;
+		}
+		$scope.createMarker=function () {
+			DG.marker($scope.newMarker.latlng).addTo($scope.markers).bindPopup($scope.newMarker.bindPopup)
+			$scope.marker.remove()
+			$scope.newMarker = { latlng: [0,0], bindPopup:"" };
+			$scope.marker=false;
+		}
+		$scope.deleteMarker=function(marker) {
+			marker.removeFrom($scope.map)
+			marker.removeFrom($scope.markers)
+		}
+		$scope.showMarker=function() {
+			$scope.markers.addTo($scope.map);
+			console.log($scope.markers)
+			console.log($scope.map)
+		}
+		$scope.hideMarker=function() {
+			$scope.markers.removeFrom($scope.map);
+		}
+
 	});
 
 
